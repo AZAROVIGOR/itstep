0be0f1365f3fb72d092806862378e8047230fa30
Azarov Igor <Azar_Off5@mail.ru>
2023-03-01 22:42:01 +0300

    Task 50. Form

--- Tasks/Task 50. Form/js/app1.js
+++ Tasks/Task 50. Form/js/app1.js
@@ -3,6 +3,7 @@ let form = document.getElementById("reg_form");
 form.addEventListener("submit", function (event) {
     event.preventDefault();
     let div = document.querySelector(".result")
+    
     let name = document.getElementById("name").value;
     div.innerHTML += `Имя: ${name}<br>`
     
@@ -20,6 +21,7 @@ form.addEventListener("submit", function (event) {
         if (element.checked){
             div.innerHTML += `Пол: ${element.value}<br>`
         }
+
     }
 
     let hobby = document.getElementsByName("hobby[]");
@@ -30,6 +32,56 @@ form.addEventListener("submit", function (event) {
     })
    
     let about = document.getElementById("about").value;
-    div.innerHTML += `О себе: ${about}`
+    div.innerHTML += `О себе: ${about}`;
+ 
+    if(name == ""){
+        let span = document.querySelector(".formName");
+        if(span){
+            span.remove();
+        }
+        let formControl = document.querySelectorAll(".form-control")[0];
+        let spanName = document.createElement("span");
+        spanName.className = "formName"
+        spanName.innerText = "Имя не введено"
+        formControl.insertAdjacentElement("beforeend", spanName);
+         document.querySelector(".formName");
+       
+    }
 
+    if (email == ""){
+        let span = document.querySelector(".formEmail");
+        if (span) {
+            span.remove();
+        }
+        let spanMail = document.createElement("span");
+        let formMail = document.querySelectorAll(".form-control")[1];
+        spanMail.className = "formEmail"
+        spanMail.innerText = "Введите email"
+        formMail.insertAdjacentElement("beforeend", spanMail);
+    }
+
+    if (tel == "") {
+        let span = document.querySelector(".formPhone");
+        if (span) {
+            span.remove();
+        }
+        let spanPhone = document.createElement("span");
+        let formPhone = document.querySelectorAll(".form-control")[2];
+        spanPhone.className = "formPhone"
+        spanPhone.innerText = "Введите номер телефона"
+        formPhone.insertAdjacentElement("beforeend", spanPhone);
+
+    }
+
+    if (education == "") {
+        let spanEducation = document.createElement("span");
+        let formEducation = document.getElementById(".education");
+        spanEducation.className = "formeducation"
+        spanEducation.innerText = "Образование не выбрано"
+        formEducation.insertAdjacentElement("beforeend", spanEducation);
+        
+    }
+    
+    
+    
 })
---
+++ Tasks/Task 50. Form/js/maincommit.js
@@ -0,0 +1,91 @@
+let form = document.getElementById("reg_form");
+
+form.addEventListener("submit", function(event){
+    event.preventDefault(); // отменяет стандартные действия браузера
+
+    // <span class="error-text">Заполните поле</span>
+
+    let errors = document.querySelectorAll(".error-text");   // Находим все элементы с классом  .error-text
+    
+    if(errors.length){                                          // если в input что- то ввели
+        Array.from(errors).forEach((errorSpan) => {                 // то у каждго найденого элемента
+            errorSpan.parentElement.classList.remove("error");    // удаляем класс  error
+            errorSpan.remove();                                     // удаляем <span></span>
+        })
+    }
+
+    let hasError = false;                   // 
+
+    let nameInput = document.querySelector("#name");
+    let emailInput = document.querySelector("#email");
+    let phoneInput = document.querySelector("#phone");
+    let aboutInput = document.querySelector("#about");
+    let educationInput = document.querySelector("#education");
+
+    let fields = [nameInput, emailInput, phoneInput, aboutInput, educationInput];  // создаем массив элементов input
+
+    fields.forEach((field) => {
+        if(field.value == ""){                                 // если поле не заполнено
+            let span = document.createElement("span");
+            span.className = "error-text"; // span.classList.add("error-text");   вешаем элементу span class "error-text"
+            span.innerText = "Заполните поле";                      // со значением  "Заполните поле"
+            field.insertAdjacentElement("afterend", span);  // в элементы input  заносим span
+            field.parentElement.classList.add("error");  //  родителю inputa  добавляем класс  "error"
+            hasError = true;  // 
+        }
+    });
+
+    let sexs = ["sex-m", "sex-f"];
+
+    let checked = sexs.some((radioId) => {                  // если хотябы один из элементов чекнут (метод some)
+        return document.querySelector("#"+radioId).checked; //document.querySelector("#"+radioId) - input    возвращаем тот элемент который чекнут
+    })
+
+    if(!checked){               // если чекбокс не чекнут
+        let span = document.createElement("span");
+        span.className = "error-text"; // span.classList.add("error-text");
+        span.innerText = "Выберите пол";
+        document.querySelector("#sex-f").parentElement.insertAdjacentElement("afterend", span);  // навешиваем span с классом error-text и текстом Выберите пол
+        hasError = true;
+    }
+
+    let hobbies = ["hobby-1", "hobby-2", "hobby-3"];
+
+    checked = hobbies.some((checkboxId) => {
+        return document.querySelector("#"+checkboxId).checked; //document.querySelector("#"+checkbox) - input возвращаем тот input который выбран
+    })
+
+    if(!checked){                                     //  если Input c хобби не выбран  
+        let span = document.createElement("span");
+        span.className = "error-text"; // span.classList.add("error-text");
+        span.innerText = "Выберите хобби";
+        document.querySelector("#hobby-3").parentElement.insertAdjacentElement("afterend", span); //навешиваем span с классом error - text и текстом "Выберите хобби"
+        hasError = true;
+    }
+
+    if (!hasError) {                                      // если hasError  везде не true
+        let div = document.querySelector(".result");
+
+        div.innerHTML += `Имя: ${nameInput.value}<br>`;    // выводим в  div . result все значения
+        div.innerHTML += `Email: ${emailInput.value}<br>`;
+        div.innerHTML += `Телефон: ${phoneInput.value}<br>`;
+        div.innerHTML += `Образование: ${educationInput.value}<br>`;
+        if (document.querySelector("#sex-m").checked) {                 //  если чекнут sex-m
+            div.innerHTML += `Пол: ${document.querySelector("#sex-m").value}<br>`;  //выводими  sex-m с его значением (("#sex-m").value)
+        } else if (document.querySelector("#sex-f").checked) {                       // если чекнут  #sex-f
+            div.innerHTML += `Пол: ${document.querySelector("#sex-f").value}<br>`;   //выводими  #sex-f с его значением (("#sex-f").value)
+        } 
+
+        let hobbyValue = [];   // создаем пустой массив в который будем помещать выбранные пользователем хобби
+
+        hobbies.forEach((hobby) => {
+            if(document.querySelector("#"+hobby).checked){                     // если выбрали хобби 
+                hobbyValue.push(document.querySelector("#" + hobby).value);   //  добавляем в  let hobbyValue = [];  выбраное хобби 
+            }
+        })
+
+        div.innerHTML += `Хобби: ${hobbyValue.join(",")}<br>`;  // в div с result помещаем  помещаем  хобби 
+        div.innerHTML += `О себе: ${aboutInput.value}<br>`;
+        this.reset(); // очищение формы this==form
+    }
+})
